3bd5da4a905df2c5d880c1d41dfd7d6cfd359a77
Daniel Rodrigues <rodriguesdaniel2403@gmail.com>
2025-08-20 21:26:03 +0530

    Dashboard sidebar

---
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
+
+import { BotIcon, StarIcon, VideoIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { DashboardUserButton } from "./dashboard-user-button";
+
+const firstSection = [
+  {
+    icon: VideoIcon,
+    label: "Meetings",
+    href: "/meetings",
+  },
+  {
+    icon: BotIcon,
+    label: "Agents",
+    href: "/agents",
+  },
+];
+
+const secondSection = [
+  {
+    icon: StarIcon,
+    label: "Upgrade",
+    href: "/upgrade",
+  },
+];
+
+export const DashboardSidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar>
+      <SidebarHeader className="text-sidebar-accent-foreground">
+        <Link href="/" className="flex items-center gap-2 px-2 pt-2">
+          <Image src="/logo.svg" height={36} width={36} alt="Clario.AI" />
+          <p className="text-2xl font-semibold">Clario.AI</p>
+        </Link>
+      </SidebarHeader>
+      <div className="px-4 py-2">
+        <Separator className="opacity-10 text-[#5D6B68]" />
+      </div>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {firstSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    className={cn(
+                      "h-10 border border-transparent hover:border-[#5D6B68]/10 hover:bg-gradient-to-r hover:from-sidebar-accent hover:from-50% hover:to-sidebar hover:to-98%",
+                      pathname === item.href &&
+                        "bg-gradient-to-r  border-[#5D6B68]/10"
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <div className="px-4 py-2">
+          <Separator className="opacity-10 text-[#5D6B68]" />
+        </div>
+        {/* Second Section */}
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {secondSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    className={cn(
+                      "h-10 border border-transparent hover:border-[#5D6B68]/10 hover:bg-gradient-to-r hover:from-sidebar-accent hover:from-50% hover:to-sidebar hover:to-98%",
+                      pathname === item.href &&
+                        "bg-gradient-to-r  border-[#5D6B68]/10"
+                    )}
+                    isActive={pathname === item.href}
+                  >
+                    <Link href={item.href}>
+                      <item.icon className="size-5" />
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter className="text-white">
+              <DashboardUserButton/>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
