f83c45c478c2705c047939cfefcaf1bf3a67f572
Daniel Rodrigues <rodriguesdaniel2403@gmail.com>
2025-08-20 23:39:06 +0530

    Dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 
 interface Props {
@@ -8,10 +9,30 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <SidebarProvider>
-      <DashboardSidebar/>
-      <main>{children}</main>
+      <div className="flex min-h-screen w-full">
+        {/* Sidebar on the left */}
+        <DashboardSidebar />
+
+        {/* Main content area */}
+        <div className="flex flex-col flex-1">
+          <DashboardNavbar />
+          <main className="flex-1 p-4">{children}</main>
+        </div>
+      </div>
     </SidebarProvider>
   );
 };
 
 export default Layout;
+
+// const Layout = ({ children }: Props) => {
+//   return (
+//     <SidebarProvider>
+//       <DashboardSidebar/>
+//       <main>
+//         <DashboardNavbar/>
+//         {children}
+//       </main>
+//     </SidebarProvider>
+//   );
+// };
