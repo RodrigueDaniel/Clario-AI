0ff392008d635893ee871dfcf180a013bb61d43e
RodrigueDaniel <149811433+RodrigueDaniel@users.noreply.github.com>
2025-08-27 17:35:25 +0530

    fix(agents-view): resolve ColumnDef type error

--- src/modules/agents/server/procedure.ts
+++ src/modules/agents/server/procedure.ts
@@ -10,20 +10,30 @@ import { z } from "zod";
 import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-    const [existingAgent] = await db.select({
-      ...getTableColumns(agents),
-      meetingCount: sql<number>`5`
-    }).from(agents).where(eq(agents.id, input.id));
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select({
+          ...getTableColumns(agents),
+          meetingCount: sql<number>`5`,
+        })
+        .from(agents)
+        .where(eq(agents.id, input.id));
 
-    //await new Promise((resolve) => setTimeout(resolve, 3000))
-    //throw new TRPCError({code: "BAD_REQUEST"})
+      //await new Promise((resolve) => setTimeout(resolve, 3000))
+      //throw new TRPCError({code: "BAD_REQUEST"})
 
-    return existingAgent;
-  }),
+      return existingAgent;
+    }),
 
   getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(agents);
+    const data = await db
+      .select({
+        ...getTableColumns(agents),
+        meetingCount: sql<number>`5`,
+      })
+      .from(agents);
 
     //await new Promise((resolve) => setTimeout(resolve, 3000))
     //throw new TRPCError({code: "BAD_REQUEST"})
