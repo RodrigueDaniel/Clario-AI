9b8556c890896fdc3cd0a69287f9d3d80d2efea9
RodrigueDaniel <149811433+RodrigueDaniel@users.noreply.github.com>
2025-08-29 20:06:25 +0530

    Agent Update Delete

--- src/modules/agents/ui/components/agent-form.tsx
+++ src/modules/agents/ui/components/agent-form.tsx
@@ -37,18 +37,79 @@ export const AgentForm = ({
     trpc.agents.create.mutationOptions({
       onSuccess: async () => {
         await queryClient.invalidateQueries(
-          trpc.agents.getMany.queryOptions({}),
-        )
+          trpc.agents.getMany.queryOptions({})
+        );
 
-        if(initialValues?.id){
-          await queryClient.invalidateQueries(
-            trpc.agents.getOne.queryOptions({ id: initialValues.id })
-          )
-        }
+        toast.success(
+          <span className="font-bold text-green-700">
+            Agent created successfully
+          </span>,
+          {
+            classNames: {
+              toast: "relative bg-white rounded-lg overflow-hidden",
+            },
+            description: (
+              <div className="absolute inset-0 rounded-lg border-2 border-green-500 animate-[borderLine_4s_linear]" />
+            ),
+          }
+        );
         onSuccess?.();
       },
       onError: (error) => {
-        toast.error(error.message);
+        toast.error(
+          <span className="font-bold text-red-700">{error.message}</span>,
+          {
+            classNames: {
+              toast: "relative bg-white rounded-lg overflow-hidden",
+            },
+            description: (
+              <div className="absolute inset-0 rounded-lg border-2 border-red-500 animate-[borderLine_4s_linear]" />
+            ),
+          }
+        );
+      },
+    })
+  );
+
+  const updateAgent = useMutation(
+    trpc.agents.update.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.agents.getMany.queryOptions({})
+        );
+
+        if (initialValues?.id) {
+          await queryClient.invalidateQueries(
+            trpc.agents.getOne.queryOptions({ id: initialValues.id })
+          );
+        }
+        toast.success(
+          <span className="font-bold text-green-700">
+            Agent updated successfully
+          </span>,
+          {
+            classNames: {
+              toast: "relative bg-white rounded-lg overflow-hidden",
+            },
+            description: (
+              <div className="absolute inset-0 rounded-lg border-2 border-green-500 animate-[borderLine_4s_linear]" />
+            ),
+          }
+        );
+        onSuccess?.();
+      },
+      onError: (error) => {
+        toast.error(
+          <span className="font-bold text-red-700">{error.message}</span>,
+          {
+            classNames: {
+              toast: "relative bg-white rounded-lg overflow-hidden",
+            },
+            description: (
+              <div className="absolute inset-0 rounded-lg border-2 border-red-500 animate-[borderLine_4s_linear]" />
+            ),
+          }
+        );
       },
     })
   );
@@ -62,59 +123,72 @@ export const AgentForm = ({
   });
 
   const isEdit = !!initialValues?.id;
-  const isPending = createAgent.isPending;
+  const isPending = createAgent.isPending || updateAgent.isPending;
 
   const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
     if (isEdit) {
-      console.log("TODO: Update");
+      updateAgent.mutate({ ...values, id: initialValues.id })
     } else {
       createAgent.mutate(values);
     }
   };
 
-  return(
+  return (
     <Form {...form}>
-        <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
-            <GeneratedAvatarProps seed={form.watch("name")} variant="botttsNeutral" className="border size-16"/>
+      <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <GeneratedAvatarProps
+          seed={form.watch("name")}
+          variant="botttsNeutral"
+          className="border size-16"
+        />
 
-            <FormField
-                name="name"
-                control={form.control}
-                render={({ field })=>(
-                    <FormItem>
-                        <FormLabel>Name</FormLabel>
-                        <FormControl>
-                            <Input {...field} placeholder="e.g. Math tutor"/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="e.g. Math tutor" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-            <FormField
-                name="instructions"
-                control={form.control}
-                render={({ field })=>(
-                    <FormItem>
-                        <FormLabel>Instructions</FormLabel>
-                        <FormControl>
-                            <Textarea {...field} placeholder="You are a helpful Math assistant that can answer questions and help with assigments"/>
-                        </FormControl>
-                        <FormMessage/>
-                    </FormItem>
-                )}
-            />
-            <div className="flex justify-between gap-x-2">
-                {onCancel && (
-                    <Button className="border border-gray-300" variant="ghost" disabled={isPending} type="button" onClick={() => onCancel()}>
-                        Cancel
-                    </Button>
-                )}
-                <Button disabled={isPending} type="submit">
-                    {isEdit ? "Update" : "Create"}
-                </Button>
-            </div>
-        </form>
+        <FormField
+          name="instructions"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Instructions</FormLabel>
+              <FormControl>
+                <Textarea
+                  {...field}
+                  placeholder="You are a helpful Math assistant that can answer questions and help with assigments"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-between gap-x-2">
+          {onCancel && (
+            <Button
+              className="border border-gray-300"
+              variant="ghost"
+              disabled={isPending}
+              type="button"
+              onClick={() => onCancel()}
+            >
+              Cancel
+            </Button>
+          )}
+          <Button disabled={isPending} type="submit">
+            {isEdit ? "Update" : "Create"}
+          </Button>
+        </div>
+      </form>
     </Form>
-  )
+  );
 };
--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -3,11 +3,20 @@
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 import { AgentIdViewHeader } from "../components/agent-id-view-header";
 import { GeneratedAvatarProps } from "@/components/generated-avatar";
 import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useState } from "react";
+import { UpdateAgentDialog } from "../components/update-agent-dialog";
 
 interface Props {
   agentId: string;
@@ -15,41 +24,110 @@ interface Props {
 
 export const AgentIdView = ({ agentId }: Props) => {
   const trpc = useTRPC();
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  const [updateAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false);
 
   const { data } = useSuspenseQuery(
     trpc.agents.getOne.queryOptions({ id: agentId })
   );
 
+  const removeAgent = useMutation(
+    trpc.agents.remove.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.agents.getMany.queryOptions({})
+        );
+
+        // Toast new Design
+        toast.success(
+          <span className="font-bold text-green-700">
+            Agent deleted successfully
+          </span>,
+          {
+            classNames: {
+              toast: "relative bg-white rounded-lg overflow-hidden",
+            },
+            description: (
+              <div className="absolute inset-0 rounded-lg border-2 border-green-500 animate-[borderLine_4s_linear]" />
+            ),
+          }
+        );
+
+        router.push("/agents");
+      },
+      onError: (error) => {
+        toast.error(
+          <span className="font-bold text-red-700">{error.message}</span>,
+          {
+            classNames: {
+              toast: "relative bg-white rounded-lg overflow-hidden",
+            },
+            description: (
+              <div className="absolute inset-0 rounded-lg border-2 border-red-500 animate-[borderLine_4s_linear]" />
+            ),
+          }
+        );
+      },
+    })
+  );
+
+  const [RemoveConfirmation, confirmRemove] = useConfirm(
+    "Are you sure?",
+    `The following action will remove ${data.meetingCount} associated meetings`
+  );
+
+  const handleRemoveAgent = async () => {
+    const ok = await confirmRemove();
+
+    if (!ok) return;
+
+    await removeAgent.mutateAsync({ id: agentId });
+  };
+
   return (
-    <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <AgentIdViewHeader 
-      agentId={agentId}
-      agentName={data.name}
-      onEdit={() => {}}
-      onRemove={() => {}}
+    <>
+      <RemoveConfirmation />
+      <UpdateAgentDialog
+        open={updateAgentDialogOpen}
+        onOpenChange={setUpdateAgentDialogOpen}
+        initialValues={data}
       />
-      <div className="bg-white rounded-lg border">
-        <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+      <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+        <AgentIdViewHeader
+          agentId={agentId}
+          agentName={data.name}
+          onEdit={() => setUpdateAgentDialogOpen(true)}
+          onRemove={handleRemoveAgent}
+        />
+        <div className="bg-white rounded-lg border">
+          <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
             <div className="flex items-center gap-x-3">
-                <GeneratedAvatarProps 
+              <GeneratedAvatarProps
                 variant="botttsNeutral"
                 seed={data.name}
                 className="size-10"
-                />
-                <h2 className="text-2xl font-medium">{data.name}</h2>
+              />
+              <h2 className="text-2xl font-medium">{data.name}</h2>
             </div>
 
-            <Badge variant="outline" className="flex items-center gap-x-2 [&>svg]:size-4">
-                <VideoIcon className="text-blue-700"/>
-                {data.meetingCount} {data.meetingCount === 1 ? "meeting" : "meetings"}
+            <Badge
+              variant="outline"
+              className="flex items-center gap-x-2 [&>svg]:size-4"
+            >
+              <VideoIcon className="text-blue-700" />
+              {data.meetingCount}{" "}
+              {data.meetingCount === 1 ? "meeting" : "meetings"}
             </Badge>
             <div className="flex flex-col gap-y-4">
-                <p className="text-lg font-medium">Instructions</p>
-                <p className="text-neutral-800">{data.instructions}</p>
+              <p className="text-lg font-medium">Instructions</p>
+              <p className="text-neutral-800">{data.instructions}</p>
             </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
