b75a8ad645b6b3436b44350134de170bd7ce9d06
RodrigueDaniel <149811433+RodrigueDaniel@users.noreply.github.com>
2025-09-04 01:33:32 +0530

    Background Jobs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Place it at the root, not inside experimental!
+  
   allowedDevOrigins: ["likely-optimum-tahr.ngrok-free.app"],
   experimental: {
     // other experimental flags go here (if any)
